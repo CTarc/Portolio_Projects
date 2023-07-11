88eb3a1d91fb62d34ebffd3c8f639e2459cdcf96
CTarc <136391087+CTarc@users.noreply.github.com>
2023-07-11 15:37:31 -0700

    Update employeeraise.sql

--- employeeraise.sql
+++ employeeraise.sql
@@ -1,8 +1,24 @@
 /*Business task given: Use data to give which employee get raises based on their sales performance.Bonues will be awarded to those employees who are responsible for the five highest order amounts.
 Find columns and tables needed for this business question.*/
 
-/*Tables needed:
+/*Tables needed from the database:
 Employees, Orders, Order details and Products tables.*/ 
+SELECT *
+FROM employees
+
+SELECT *
+FROM orders
+
+SELECT * 
+FROM orderdetails
+
+SELECT *
+FROM products
+
+ -- Checked tables for common columns
+
+
+ -- Used Join statement on the common columns
 SELECT lastname, firstname, orders.orderid, products.productid,quantity, price 
 FROM employees
 	INNER JOIN orders 
